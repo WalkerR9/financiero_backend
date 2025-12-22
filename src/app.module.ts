2cd1a8aa705b83da5b046b4579f6d242665ab486
WalkerR9 <romerow169@gmail.com>
2025-12-22 13:58:09 -0600

    setting update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const validateEnv = () => {
+  if (!process.env.DATABASE_HOST || !process.env.DATABASE_PORT) {
+    throw new Error('Missing required environment variables');
+  }
+};
+
+validateEnv();
+
 
 @Module({
-  imports: [],
+  imports: [
+     TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT ?? '3306', 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
